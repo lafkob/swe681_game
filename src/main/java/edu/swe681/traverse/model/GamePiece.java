8af822eb8d058fa2801bf5ac9b9d469f2a35f7e0
lafkob <lafkob@gmail.com>
2016-03-16 19:10:40 -0400

    extracted the piece type enum

--- src/main/java/edu/swe681/traverse/model/GamePiece.java
+++ src/main/java/edu/swe681/traverse/model/GamePiece.java
@@ -4,13 +4,13 @@ package edu.swe681.traverse.model;
  * Represents a traverse game piece
  */
 public class GamePiece {
-	private final PieceType type;
+	private final GamePieceType pieceType;
 
-	public GamePiece(PieceType type) {
-		this.type = type;
+	public GamePiece(GamePieceType pieceType) {
+		this.pieceType = pieceType;
 	}
-
-	public static enum PieceType {
-		SQUARE, CIRCLE, DIAMOND, TRIANGLE;
+	
+	public GamePieceType getPieceType() {
+		return this.pieceType;
 	}
 }
---
+++ src/main/java/edu/swe681/traverse/model/GamePieceType.java
@@ -0,0 +1,5 @@
+package edu.swe681.traverse.model;
+
+public enum GamePieceType {
+	SQUARE, CIRCLE, DIAMOND, TRIANGLE;
+}
