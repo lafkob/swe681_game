e7b12860cdab3f922883d43f2ab14ea009529773
Storm Lazo <kllazo@live.com>
2016-03-21 12:40:55 -0400

    Initial logging commit
    Added SLF4J dependencies to build.gradle
    Added initial logback.xml
    Added a log statement to PingController (just to test it)

--- src/main/java/edu/swe681/traverse/example/PingController.java
+++ src/main/java/edu/swe681/traverse/example/PingController.java
@@ -5,12 +5,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Controller
 public class PingController {
 
+	private static final Logger LOG = 
+			LoggerFactory.getLogger(PingController.class);
+	
 	@RequestMapping(value="/api/ping", method = RequestMethod.GET)
 	@ResponseBody
 	public String ping(){
+		
 		return "System up";
 	}
 }
