6dc3be5579c54c56c6d474afa68b9e612ede5b4e
lafkob <lafkob@gmail.com>
2016-04-13 22:37:33 -0400

    basic model to mirror the user table

---
+++ src/main/java/edu/swe681/traverse/model/UserModel.java
@@ -0,0 +1,99 @@
+package edu.swe681.traverse.model;
+
+/**
+ * POJO to represent the User model.
+ */
+public class UserModel {
+	private long id;
+	private String username;
+	private String passwordHash;
+	private String passwordSalt;
+	
+	public UserModel(){}
+
+	public UserModel(long id, String username, String passwordHash, String passwordSalt) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.passwordHash = passwordHash;
+		this.passwordSalt = passwordSalt;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getPasswordSalt() {
+		return passwordSalt;
+	}
+
+	public void setPasswordSalt(String passwordSalt) {
+		this.passwordSalt = passwordSalt;
+	}
+
+	@Override
+	public String toString() {
+		return "UserModel [id=" + id + ", username=" + username + ", passwordHash=" + passwordHash + ", passwordSalt="
+				+ passwordSalt + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((passwordHash == null) ? 0 : passwordHash.hashCode());
+		result = prime * result + ((passwordSalt == null) ? 0 : passwordSalt.hashCode());
+		result = prime * result + ((username == null) ? 0 : username.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UserModel other = (UserModel) obj;
+		if (id != other.id)
+			return false;
+		if (passwordHash == null) {
+			if (other.passwordHash != null)
+				return false;
+		} else if (!passwordHash.equals(other.passwordHash))
+			return false;
+		if (passwordSalt == null) {
+			if (other.passwordSalt != null)
+				return false;
+		} else if (!passwordSalt.equals(other.passwordSalt))
+			return false;
+		if (username == null) {
+			if (other.username != null)
+				return false;
+		} else if (!username.equals(other.username))
+			return false;
+		return true;
+	}
+}
