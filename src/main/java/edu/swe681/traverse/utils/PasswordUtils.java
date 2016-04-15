baa7b5a05b999c087b73d2e8478f37008986d3dc
Storm Lazo <kllazo@live.com>
2016-04-14 21:45:22 -0400

    Updated password util file
    Updated password util file to use Spring security BCrypt utility.

--- src/main/java/edu/swe681/traverse/utils/PasswordUtils.java
+++ src/main/java/edu/swe681/traverse/utils/PasswordUtils.java
@@ -1,31 +1,22 @@
 package edu.swe681.traverse.utils;
 
-import java.util.Date;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Utilities for handling passwords
  */
 public class PasswordUtils {
-
-	/**
-	 * Generates a salt to be used in password hashing.
-	 * 
-	 * @return A salt
-	 */
-	public static String generateSalt() {
-		// TODO:
-		return new Date().toString();
-	}
 	
 	/**
-	 * Creates a hash from a password and salt.
+	 * Creates a hash from a password.
 	 * 
 	 * @param password
-	 * @param salt
 	 * @return
 	 */
-	public static String hashPassword(String password, String salt) {
-		// TODO
-		return password + salt;
+	public static String hashPassword(String password) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String hashedPassword = passwordEncoder.encode(password);
+		
+		return hashedPassword;
 	}
 }
