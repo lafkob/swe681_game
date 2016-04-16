5c98766ced8494777521d0941c0a62c270d70098
lafkob <lafkob@gmail.com>
2016-04-16 00:39:35 -0400

    todos to address

--- src/main/java/edu/swe681/traverse/game/enums/GameStatus.java
+++ src/main/java/edu/swe681/traverse/game/enums/GameStatus.java
@@ -4,5 +4,7 @@ package edu.swe681.traverse.game.enums;
  * Represents a particular state of the game.
  */
 public enum GameStatus {
+	// TODO: what do each of these mean?
+	// TODO: do we need a finished state rather than WIN?
 	WAITING_FOR_PLAYERS, START, WIN, PLAY;
 }
