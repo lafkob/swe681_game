976c8f3d0d043d11c5761585db7888269df8cf8a
lafkob <lafkob@gmail.com>
2016-04-18 19:04:16 -0400

    skip autowire encoder, it is only used here

--- src/main/java/edu/swe681/traverse/application/config/SecurityConfig.java
+++ src/main/java/edu/swe681/traverse/application/config/SecurityConfig.java
@@ -3,10 +3,8 @@ package edu.swe681.traverse.application.config;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,27 +17,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter
 	@Autowired
 	DataSource dataSource;
 	
+	// TODO: Need to pick a strength we think is good. I read somewhere
+	// that 12 is the least we'd want to go, though I've seen 10 in a lot of places
+	// too...
+	private final PasswordEncoder encoder = new BCryptPasswordEncoder(12);
+	
 	@Autowired
 	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
 		
 	  auth.jdbcAuthentication()
 	  	.dataSource(dataSource)
-	  	.passwordEncoder(passwordEncoder())
+	  	.passwordEncoder(encoder)
 		.usersByUsernameQuery(
 			"SELECT username,password_hash,enabled FROM users WHERE username=?")
 		.authoritiesByUsernameQuery(
 			"SELECT username, role FROM user_roles WHERE username=?");
-	}
-	
-	//TODO: Could theoretically move this to RootConfiguration if we really want to? I'm still getting
-	// the hang of how AutoWired functions, so maybe that wouldn't work..
-	@Bean
-	public PasswordEncoder passwordEncoder()
-	{
-		// TODO: Need to pick a strength we think is good. I read somewhere
-		// that 12 is the least we'd want to go, though I've seen 10 in a lot of places
-		// too...
-		PasswordEncoder encoder = new BCryptPasswordEncoder(12);
-		return encoder;
 	}
 }
