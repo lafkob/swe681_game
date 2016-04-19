0df52bb69b5a561e33e9dc5bde0d278411eed5e9
lafkob <lafkob@gmail.com>
2016-04-18 20:58:20 -0400

    no auth for registration page and api

--- src/main/java/edu/swe681/traverse/application/config/SecurityConfig.java
+++ src/main/java/edu/swe681/traverse/application/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,16 +29,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.authoritiesByUsernameQuery("SELECT username, role FROM user_roles WHERE username=?");
 	}
 
-//	@Override
-//	protected void configure(HttpSecurity http) throws Exception {
-//		http
-//			.authorizeRequests()
-//				.antMatchers("/api/register").anonymous()
-//				.anyRequest()
-//				.authenticated()
-//				.and()
-//			.formLogin()
-//				.loginPage("/login.jsp")
-//				.permitAll();
-//	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/register.jsp", "/api/register", "/js/**", "/css/**").permitAll()
+				.anyRequest().authenticated()
+				.and()
+			.formLogin().and()
+			.httpBasic();
+	}
 }
