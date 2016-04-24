4f4378f040977cd1548a1145aa0df08a1cd05b4a
lafkob <lafkob@gmail.com>
2016-04-24 16:14:39 -0400

    some more fleshing out of main methods

--- src/main/java/edu/swe681/traverse/rest/GameRestController.java
+++ src/main/java/edu/swe681/traverse/rest/GameRestController.java
@@ -14,11 +14,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import edu.swe681.traverse.application.exception.BadRequestException;
 import edu.swe681.traverse.application.exception.InternalServerException;
 import edu.swe681.traverse.application.exception.NotFoundException;
 import edu.swe681.traverse.application.exception.NotYetImplementedException;
 import edu.swe681.traverse.game.GameBoard;
+import edu.swe681.traverse.game.exception.TraverseException;
 import edu.swe681.traverse.model.GameModel;
 import edu.swe681.traverse.persistence.dao.GamesDao;
 import edu.swe681.traverse.persistence.dao.UsersDao;
@@ -112,9 +115,39 @@ public class GameRestController {
 	// TODO: document
 	@RequestMapping(value="/join", method = RequestMethod.POST)
 	@ResponseBody
-	public GameStatusResponseDto joinGame(@Valid @RequestBody JoinRequestDto joinRequest) throws NotYetImplementedException {
-		// TODO: get the user and make sure they are not already in a game
-		// TODO: update the game state to show this user in it
+	public GameStatusResponseDto joinGame(@Valid @RequestBody JoinRequestDto dto, Principal principal)
+			throws NotYetImplementedException, BadRequestException, NotFoundException, InternalServerException, TraverseException {
+		final GameModel game = gamesDao.getGameById(dto.getGameId());
+		
+		if(game == null) {
+			throw new NotFoundException("Game not found");
+		}
+		
+		final long userId = usersDao.getUserByUsername(principal.getName()).getId();
+		
+		// make sure user is not already in a game
+		if (gamesDao.isPlayerCurrentlyInAGame(userId)) {
+			// TODO: audit this?
+			throw new BadRequestException("User already in a game");
+		}
+		
+		GameBoard board = null;
+		try {
+			board = new GameBoard(game);
+		} catch (IOException e) {
+			// TODO: some kind of log
+			throw new InternalServerException("Bad game state, please contact an admin");
+		}
+		
+		board.registerPlayerTwo(userId);
+		
+		try {
+			gamesDao.updateGame(new GameModel(board));
+		} catch (JsonProcessingException e) {
+			// TODO: some kind of log
+			throw new InternalServerException("Bad game state, please contact an admin");
+		}
+		
 		// TODO: auditing stuff
 		// TODO: return the full game state
 		
@@ -127,8 +160,18 @@ public class GameRestController {
 	// TODO: document
 	@RequestMapping(value="/status", method = RequestMethod.POST)
 	@ResponseBody
-	public GameStatusResponseDto getStatus(@Valid @RequestBody StatusRequestDto statusRequest) throws NotYetImplementedException {
-		// TODO: get the user and make sure they are in the game being requested
+	public GameStatusResponseDto getStatus(@Valid @RequestBody StatusRequestDto dto, Principal principal)
+			throws NotYetImplementedException, NotFoundException {
+		final GameModel game = gamesDao.getGameById(dto.getGameId());
+		final long userId = usersDao.getUserByUsername(principal.getName()).getId();
+		
+		// make sure the user is in the game, don't give a specific error as
+		// to whether the game exists
+		if (game == null || !game.isUserInGame(userId)) {
+			// TODO: audit this?
+			throw new NotFoundException("No game found for user");
+		}
+		
 		// TODO: get the game state and populate it fully
 		// TODO: audit the call?
 
@@ -142,8 +185,18 @@ public class GameRestController {
 	// TODO: document
 	@RequestMapping(value="/move", method = RequestMethod.POST)
 	@ResponseBody
-	public GameStatusResponseDto makeMove(@Valid @RequestBody MoveRequestDto moveRequest) throws NotYetImplementedException {
-		// TODO: get the user and make sure they are in the game for the move
+	public GameStatusResponseDto makeMove(@Valid @RequestBody MoveRequestDto dto, Principal principal)
+			throws NotYetImplementedException, NotFoundException {
+		final GameModel game = gamesDao.getGameById(dto.getGameId());
+		final long userId = usersDao.getUserByUsername(principal.getName()).getId();
+		
+		// make sure the user is in the game, don't give a specific error as
+		// to whether the game exists
+		if (game == null || !game.isUserInGame(userId)) {
+			// TODO: audit this?
+			throw new NotFoundException("No game found for user");
+		}
+		
 		// TODO: update the game state, exceptions will be thrown and handled globally
 		// TODO: audit the call
 
