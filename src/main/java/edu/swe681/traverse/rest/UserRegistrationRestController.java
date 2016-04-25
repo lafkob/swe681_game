5c35aaea4de25586f6c7b00537b545f1926b705d
lafkob <lafkob@gmail.com>
2016-04-25 19:23:31 -0400

    added logging for registratoin controller

--- src/main/java/edu/swe681/traverse/rest/UserRegistrationRestController.java
+++ src/main/java/edu/swe681/traverse/rest/UserRegistrationRestController.java
@@ -2,6 +2,8 @@ package edu.swe681.traverse.rest;
 
 import javax.validation.Valid;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +25,8 @@ import edu.swe681.traverse.rest.dto.request.UserRegistrationRequestDto;
 @RestController
 @RequestMapping(value="/api/register")
 public class UserRegistrationRestController {
+
+	private static final Logger LOG = LoggerFactory.getLogger(UserRegistrationRestController.class);
 	
 	@Autowired
 	private UsersDao usersDao;
@@ -40,14 +44,17 @@ public class UserRegistrationRestController {
 	public ResponseEntity<String> registerUser(@RequestBody @Valid UserRegistrationRequestDto requestDto) throws BadRequestException {
 		
 		if(usersDao.doesUsernameExist(requestDto.getUsername())) {
+			LOG.info("Attempt to register username that is already in use: " + requestDto.getUsername());
 			throw new BadRequestException("Username is already in use");
 		}
 		
 		if(!requestDto.getPassword().equals(requestDto.getPasswordConfirm())) {
+			LOG.info("Attempt to register with non-matching passwords for user: " + requestDto.getUsername());
 			throw new BadRequestException("Passwords do not match");
 		}
 		
 		usersDao.saveUser(requestDto.getUsername(), encoder.encode(requestDto.getPassword()));
+		LOG.info("Registered new user! Username: " + requestDto.getUsername());
 		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
