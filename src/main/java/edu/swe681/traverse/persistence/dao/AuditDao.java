89d2c5bc25d307527b3f326c4db0e8e60df900de
lafkob <lafkob@gmail.com>
2016-04-25 19:21:09 -0400

    removed unused code in auditdao

--- src/main/java/edu/swe681/traverse/persistence/dao/AuditDao.java
+++ src/main/java/edu/swe681/traverse/persistence/dao/AuditDao.java
@@ -26,15 +26,6 @@ public class AuditDao
 	private final String FIND_BY_GAME_ID = "SELECT ID, GAME_ID, TIMESTAMP, PLAYER_ID, "
 			+ "PIECE_ID, MOVE FROM audit WHERE GAME_ID = ? ORDER BY TIMESTAMP DESC";
 	
-	/*
-	 * private final static String ID_COL = "ID";
-		private final static String GAME_ID_COL = "GAME_ID";
-		private final static String TIMESTAMP_COL = "TIMESTAMP";
-		private final static String PLAYER_ID_COL = "PLAYER_ID";
-		private final static String PIECE_ID_COL = "PIECE_ID";
-		private final static String MOVE_COL = "MOVE";
-	 */
-	
 	private final JdbcTemplate jdbcTemplate;
 	private final AuditRowMapper mapper;
 	
