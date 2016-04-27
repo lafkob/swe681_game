19d2d0ea171e56e401d22637ec925fb6736450b0
lafkob <lafkob@gmail.com>
2016-04-27 00:19:43 -0400

    forfeit query done :(

--- src/main/java/edu/swe681/traverse/timer/ForfeitScheduledJob.java
+++ src/main/java/edu/swe681/traverse/timer/ForfeitScheduledJob.java
@@ -1,9 +1,19 @@
 package edu.swe681.traverse.timer;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Objects;
+
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import edu.swe681.traverse.game.enums.GameStatus;
 
 /**
  * A timer class that will run and automatically forfeit a user if they have
@@ -14,12 +24,42 @@ public class ForfeitScheduledJob {
 	private static final Logger LOG = LoggerFactory.getLogger(ForfeitScheduledJob.class);
 	
 	/**
-	 * Forfeit process method. Takes 5 minutes to fire after startup and runs every 5 minutes after that.
+	 * Conditional select to get the player who is NOT the current player.
+	 */
+	private final static String SELECT_NON_CURRENT_PLAYER = 
+			"SELECT CASE WHEN PLAYER1_ID = CURRENT_PLAYER_ID THEN PLAYER2_ID ELSE PLAYER1_ID END as WINNER_ID";
+	
+	private final static String SELECT_EXPIRED_GAME_IDS = 
+			"select game_id from (select max(game_id) as game_id, max(timestamp) from audit a where a.timestamp < ? group by a.game_id) as game_id";
+	
+	private final static String FORFEIT_EXPIRED_GAMES = 
+			"UPDATE GAMES SET CURRENT_PLAYER_ID = ("+ SELECT_NON_CURRENT_PLAYER + "), STATUS = '" + GameStatus.FORFEIT +
+			"' WHERE ID IN (" + SELECT_EXPIRED_GAME_IDS + ") AND STATUS = '" + GameStatus.PLAY + "'";
+	
+	private final static int EXPIRATION_MINUTES = 5;
+	
+	private final JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public ForfeitScheduledJob(DataSource datasource)
+	{
+		Objects.requireNonNull(datasource, "datasource required");
+		this.jdbcTemplate = new JdbcTemplate(datasource);
+	}
+	
+	/**
+	 * Forfeit process method. Fires on startup and every 5 minutes after that.
 	 */
 	@Scheduled(fixedRate = 300000)
 	public void processForfeits() {
 		LOG.info("Running forfeit process job");
-		// System.out.println("Running forfeit process job");
-		// TODO: algorithm to cancel expired games 
+		Calendar cal = Calendar.getInstance();
+		cal.add(Calendar.MINUTE, 0 - EXPIRATION_MINUTES);
+		
+		int updated = jdbcTemplate.update(FORFEIT_EXPIRED_GAMES, new Timestamp(cal.getTimeInMillis()));
+		LOG.info("Forfeited " + updated + "games");
+		
+		// TODO: remove
+//		System.out.println("Forfeited " + updated + " games");
 	}
 }
