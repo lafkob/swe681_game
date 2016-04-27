fc0a1a6e3c87491432d678441556e5b8c9026da7
lafkob <lafkob@gmail.com>
2016-04-26 21:44:57 -0400

    fix to allow players to see game audit in any state

--- src/main/java/edu/swe681/traverse/rest/GameInfoRestController.java
+++ src/main/java/edu/swe681/traverse/rest/GameInfoRestController.java
@@ -16,8 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.swe681.traverse.application.exception.BadRequestException;
 import edu.swe681.traverse.game.enums.GameStatus;
 import edu.swe681.traverse.model.GameModel;
+import edu.swe681.traverse.model.UserModel;
 import edu.swe681.traverse.persistence.dao.AuditDao;
 import edu.swe681.traverse.persistence.dao.GamesDao;
+import edu.swe681.traverse.persistence.dao.UsersDao;
 import edu.swe681.traverse.rest.dto.request.GameRequestDto;
 import edu.swe681.traverse.rest.dto.response.GameAuditListResponseDto;
 import edu.swe681.traverse.rest.dto.response.GameListResponseDto;
@@ -36,6 +38,9 @@ public class GameInfoRestController {
 	
 	@Autowired
 	private AuditDao auditDao;
+	
+	@Autowired
+	private UsersDao usersDao;
 	
 	@RequestMapping(value="/open-games", method = RequestMethod.POST)
 	@ResponseBody
@@ -61,8 +66,12 @@ public class GameInfoRestController {
 			throws BadRequestException {
 		// only allow if the game is finished!
 		final GameModel game = gamesDao.getGameById(dto.getGameId());
-		if (game.getGameStatus() != GameStatus.WIN && game.getGameStatus() != GameStatus.FORFEIT
-				&& game.getGameStatus() != GameStatus.ENDED) {
+		final UserModel user = usersDao.getUserByUsername(principal.getName());
+		
+		// only allow the history to be returned under one of the following conditions:
+		// 1. the requesting user is in the game
+		// 2. the game has concluded (for non-players to see)
+		if (!game.isUserInGame(user.getId()) && !isGameInFinalState(game)) {
 			LOG.info("Attempt to view game audit for game in progress. User: " + principal.getName() + " GameID: "
 					+ dto.getGameId());
 			throw new BadRequestException("Game history not found");
@@ -71,5 +80,8 @@ public class GameInfoRestController {
 		return new GameAuditListResponseDto(dto.getGameId(), auditDao.getAuditsByGameId(dto.getGameId()));
 	}
 
-	// TODO: way to get the audit list for a given game
+	private boolean isGameInFinalState(GameModel game) {
+		return game.getGameStatus() == GameStatus.WIN || game.getGameStatus() == GameStatus.FORFEIT
+				|| game.getGameStatus() == GameStatus.ENDED;
+	}
 }
