939b840fe8d8a28408d7c8d8f1ffc688870bd726
lafkob <lafkob@gmail.com>
2016-04-27 20:14:32 -0400

    index board drawing, js refactor
    -board and id key will draw if board data is returned
    -refactored a bunch of common ajax code
    -slight display changes aside from board display

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,109 +1,36 @@
 $(function() {
 	$('#startBtn').click(function() {
-		reqData = {};
-		$.ajax({
-			type : "POST",
-			url : "./api/game/start",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			dataType : 'json',
-			contentType: 'application/json'
-		});
+		utils.ajaxPostNoParams("./api/game/start");
 		return false;
 	});
 	
 	$('#openGamesBtn').click(function() {
-		reqData = {};
-		$.ajax({
-			type : "POST",
-			url : "./api/info/open-games",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			dataType : 'json',
-			contentType: 'application/json'
-		});
+		utils.ajaxPostNoParams("./api/info/open-games");
 		return false;
 	});
 	
 	$('#finishedGamesBtn').click(function() {
-		reqData = {};
-		$.ajax({
-			type : "POST",
-			url : "./api/info/finished-games",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			dataType : 'json',
-			contentType: 'application/json'
-		});
+		utils.ajaxPostNoParams("./api/info/finished-games");
 		return false;
 	});
 	
 	$('#userInfoBtn').click(function() {
-		reqData = {};
-		$.ajax({
-			type : "POST",
-			url : "./api/info/users",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			dataType : 'json',
-			contentType: 'application/json'
-		});
+		utils.ajaxPostNoParams("./api/info/users");
 		return false;
 	});
 	
 	$('#auditBtn').click(function() {
-		reqData = {};
-		reqData.gameId = $("#auditGameId").val();
-		$.ajax({
-			type : "POST",
-			url : "./api/info/game",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			dataType : 'json',
-			contentType: 'application/json'
-		});
+		utils.ajaxPostGameIdOnly("./api/info/game", $("#auditGameId").val());
 		return false;
 	});
 	
 	$('#joinBtn').click(function() {
-		reqData = {};
-		reqData.gameId = $("#gameId").val();
-		$.ajax({
-			type : "POST",
-			url : "./api/game/join",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			dataType : 'json',
-			contentType: 'application/json'
-		});
+		utils.ajaxPostGameIdOnly("./api/game/join", $("#gameId").val());
 		return false;
 	});
 
 	$('#statusBtn').click(function() {
-		reqData = {};
-		reqData.gameId = $("#gameId").val();
-		$.ajax({
-			type : "POST",
-			url : "./api/game/status",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			dataType : 'json',
-			contentType: 'application/json'
-		});
+		utils.ajaxPostGameIdOnly("./api/game/status", $("#gameId").val());
 		return false;
 	});
 
@@ -124,17 +51,7 @@ $(function() {
 	});
 	
 	$('#quitBtn').click(function() {
-		reqData = {};
-		reqData.gameId = $("#gameId").val();
-		$.ajax({
-			type : "POST",
-			url : "./api/game/quit",
-			data : JSON.stringify(reqData),
-			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
-			success : utils.jsonSuccessHandler,
-			error: utils.requestErrorHandler,
-			contentType: 'application/json'
-		});
+		utils.ajaxPostGameIdOnly("./api/game/quit", $("#gameId").val());
 		return false;
 	});
 
@@ -154,18 +71,92 @@ $(function() {
 });
 
 utils = {
+	ajaxPostNoParams: function(url) {
+		reqData = {};
+		$.ajax({
+			type : "POST",
+			url : url,
+			data : JSON.stringify(reqData),
+			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
+			success : utils.jsonSuccessHandler,
+			error: utils.requestErrorHandler,
+			dataType : 'json',
+			contentType: 'application/json'
+		});
+	},
+	
+	ajaxPostGameIdOnly: function(url, gameId) {
+		reqData = {};
+		reqData.gameId = gameId;
+		$.ajax({
+			type : "POST",
+			url : url,
+			data : JSON.stringify(reqData),
+			headers: {'X-CSRF-TOKEN': $("input#_csrf_token").val()},
+			success : utils.jsonSuccessHandler,
+			error: utils.requestErrorHandler,
+			contentType: 'application/json'
+		});
+	},
+		
 	requestErrorHandler: function(jqXHR, textStatus, errorThrown){
-		var output = "<b>Request Error</b><br/>";
-		output += jqXHR.status + ": " + errorThrown + "<br/>";
-		if(jqXHR.responseJSON) output += JSON.stringify(jqXHR.responseJSON);
+		var output = "<b>Status:</b> ";
+		output += jqXHR.status + " (" + errorThrown + ")<br/>";
+		if(jqXHR.responseJSON) output += "<b>Response:</b> " + JSON.stringify(jqXHR.responseJSON);
 		// do not show responseText in case html is returned
 		
 		$("div#reqResult").html(output);
 	},
 	
 	jsonSuccessHandler: function(data, status, jqXHR){
-		message = "<b>Status:</b> " + jqXHR.status + " (" + status + ")";
-		if(data) message += "<br/>" + JSON.stringify(data);
+		var message = "<b>Status:</b> " + jqXHR.status + " (" + status + ")";
+		
+		if(data) message += "<br/><b>Data:</b> " + JSON.stringify(data);
+		
+		if(data.board) message += "<br/><br/>" + utils.createBoardDisplay(data.board);
+		
 		$("div#reqResult").html(message);
+	},
+	
+	createBoardDisplay: function(board2dArray) {
+		var boardDisplay = "<b>Board Display</b><br/>";
+		boardDisplay += "(0,0 in bottom left)<br/>";
+		boardDisplay += "<div>"; // wrap the two panels
+		boardDisplay += "<div class=\"leftPanel\">"; // board panel
+		boardDisplay += "<table class=\"board\">";
+
+		for (var x = board2dArray.length - 1; x >= 0; x--) {
+			boardDisplay += "<tr>";
+			for (var y = 0; y < board2dArray[x].length; y++) {
+				boardDisplay += "<td class=\"cell\"><div class=\"cell\">";
+				if(board2dArray[x][y] >= 0) {
+					boardDisplay += board2dArray[x][y];
+				}
+				boardDisplay += "</div></td>";
+			}
+		    boardDisplay += "</tr>";
+		}
+		
+		boardDisplay += "</table>";
+		boardDisplay += "</div>";
+		boardDisplay += utils.createBoardKeyDisplay();
+		boardDisplay += "</div>";
+		return boardDisplay;
+	},
+	
+	createBoardKeyDisplay: function() {
+		var key = "<div class=\"rightPanel\">";
+		key += "<b>Player 1 Piece Mappings</b><br/>";
+		key += "0, 1 -> Circle Piece<br/>";
+		key += "2, 3 -> Diamond Piece<br/>";
+		key += "4, 5 -> Square Piece<br/>";
+		key += "6, 7 -> Triangle Piece<br/><br/>";
+		key += "<b>Player 2 Piece Mappings</b><br/>";
+		key += "8, 9 -> Circle Piece<br/>";
+		key += "10, 11 -> Diamond Piece<br/>";
+		key += "12, 13 -> Square Piece<br/>";
+		key += "14, 15 -> Triangle Piece<br/>";
+		key += "</div>"
+		return key;
 	}
 };
