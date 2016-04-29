9eea1c94b4da3b9136576c3d6340d07798c58549
lafkob <lafkob@gmail.com>
2016-04-29 16:57:29 -0400

    ping no longer requires auth

--- src/main/java/edu/swe681/traverse/application/config/SecurityConfig.java
+++ src/main/java/edu/swe681/traverse/application/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/register.jsp", "/api/register", "/js/**", "/css/**").permitAll()
+				.antMatchers("/register.jsp", "/api/register", "/js/**", "/css/**", "/api/ping").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin().and()
