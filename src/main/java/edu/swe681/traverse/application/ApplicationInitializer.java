0e188c60da34d7307fcec49a299428a3d64e0387
lafkob <lafkob@gmail.com>
2016-05-01 15:18:39 -0400

    force session tracking via cookies

--- src/main/java/edu/swe681/traverse/application/ApplicationInitializer.java
+++ src/main/java/edu/swe681/traverse/application/ApplicationInitializer.java
@@ -1,6 +1,11 @@
 package edu.swe681.traverse.application;
 
+import java.util.EnumSet;
+
 import javax.servlet.Filter;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.SessionTrackingMode;
 
 import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
@@ -9,6 +14,13 @@ import edu.swe681.traverse.application.config.RootConfiguration;
 import edu.swe681.traverse.application.config.WebMvcConfiguration;
 
 public class ApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		// this will prevent URLs from ever showing the JSESSIONID, they only go in cookies
+		servletContext.setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+		super.onStartup(servletContext);
+	}
+	
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class[] { RootConfiguration.class };
